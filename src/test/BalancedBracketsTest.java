75fe4a78c513415f3b7286320b2f5399a4669f3f
msajsnow <84166958+msajsnow@users.noreply.github.com>
2021-08-12 18:30:12 -0500

    added tests

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -9,8 +10,33 @@ public class BalancedBracketsTest {
     //TODO: add tests here
     @Test
     public void emptyTest() {
+
         assertEquals(true, true);
     }
 
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+    @Test
+    public void noBracketsStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+    @Test
+    public void bracketsInsideOfString() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("String[string]"));
+    }
+    @Test
+    public void bracketsBeforeString() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]String"));
+    }
+    @Test
+    public void onlyOneOpenBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[String"));
+    }
+    @Test
+    public void backwardsBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("String]string["));
+    }
 
 }
